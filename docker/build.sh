efc34bc9024791f75551fad6ec72057da28a61ff
stanleyguo0207 <stanleyguo0207@163.com>
2023-08-04 00:38:36 +0800

    feat: 开发环境脚本更新
    Signed-off-by: stanleyguo0207 <stanleyguo0207@163.com>

--- docker/build.sh
+++ docker/build.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-# For example:
-#   bash build.sh -f bookworm.Dockerfile -i gamemaster -t 1.0.0
-#   bash build.sh -f bookworm.Dockerfile -i gamemaster -t 1.0.0 -a GAMEMASTER_USER=gamemaster -a GAMEMASTER_HOME=/opt/docker_home
-
 usage() {
   cat <<EOF
 
-Usage: bash build.sh -f docker_file -i image_name -t image_tag -a build_arg -a build_arg
+Usage: bash $0 -f <file> [-i <string>] [-t <string>] [-a <string>=<string> ...] [-c <string>] [-p <uint16|>9000>]
+
+E.g.
+    bash $0 -f bookworm.Dockerfile -i gamemaster -t 1.0.0 -a GAMEMASTER_USER=gamemaster -a GAMEMASTER_HOME=/opt/docker_home -c stanley -p 9901
 
 EOF
+  exit 1
 }
 
 image_name="gamemaster"
 image_tag="1.0.0"
 
-while getopts ":f:i:t:a:h:" arg; do
+while getopts ":f:i:t:a:c:p:" arg; do
   case $arg in
   f)
     docker_file=${OPTARG}
@@ -29,20 +29,80 @@ while getopts ":f:i:t:a:h:" arg; do
   a)
     build_arg="$build_arg--build-arg ${OPTARG} "
     ;;
-  h)
-    usage
-    exit 1
+  c)
+    container_name=${OPTARG}
+    ;;
+  p)
+    ssh_port=${OPTARG}
     ;;
   \?)
-    echo "invalid flag"
-    exit 1
+    echo "Invalid parameter     -${OPTARG}"
+    usage
     ;;
   esac
 done
 
-build_arg="$build_arg--build-arg GAMEMASTER_VERSION=$image_tag"
+if [[ ! -f $docker_file ]]; then
+  echo "Dockerfile not found      $docker_file"
+  usage
+fi
 
-docker login
-docker pull hello-world:latest
-docker pull gcc:13-bookworm
-docker build --network=host -f $docker_file -t $image_name:$image_tag $build_arg .
+if [[ -z $image_name ]]; then
+  echo "Docker build image name is empty"
+  usage
+fi
+
+if [[ -z $image_tag ]]; then
+  echo "Docker build image tag is empty"
+  usage
+fi
+
+if [[ -z $container_name ]]; then
+  echo "Docker create container name is empty"
+  usage
+fi
+
+if [[ $ssh_port -le 9000 ]]; then
+  echo "Docker create container port is invalid, must greater than 9000"
+  usage
+fi
+
+check_info=$(docker ps -a | grep "$ssh_port->22")
+
+if [[ -n $check_info ]]; then
+  echo "Docker create container port already used by other container"
+  echo "$check_info"
+  usage
+fi
+
+image_hash=$(docker images -q "$image_name:$image_tag")
+container_hash=$(docker ps -aqf "name=$container_name")
+
+echo "Build info:"
+echo "-----------"
+echo "docker_file:        $docker_file"
+echo "image_name:         $image_name"
+echo "image_tag:          $image_tag"
+echo "image:              $image_name:$image_tag"
+echo "image_hash:         $image_hash"
+echo "build_arg:          $build_arg"
+echo "container_name:     $container_name"
+echo "container_hash:     $container_hash"
+echo "ssh_port:           $ssh_port"
+echo "-----------"
+
+if [[ -z $image_hash ]]; then
+  echo ""
+  echo "-----------"
+  echo "Building image..."
+
+  docker login
+  docker pull hello-world:latest
+  docker pull gcc:13-bookworm
+  docker build --network=host --no-cache -f $docker_file -t $image_name:$image_tag $build_arg .
+fi
+
+if [[ -z $container_hash ]]; then
+  docker create --name $container_name -p $ssh_port:22 --security-opt seccomp=unconfined --privileged=true --restart=always $image_name:$image_tag /sbin/init
+  docker start $container_name
+fi
